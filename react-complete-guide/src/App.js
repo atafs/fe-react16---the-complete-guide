2a00ab133bfe0fdfb0c4cd03b46ae7479c82f44e
Americo Tomas <americo.tomas@fuseuniversal.com>
2018-07-19 20:24:16 +0100

    creating a functional component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Person from './Person/Person'
+
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>I AM A REACT APP!!</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'M A REACT APP!!'))
+    return (
+      <div className="App">
+        <h1>I AM A REACT APP!!</h1>
+        <p>this is really working!!</p>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'M A REACT APP!!'))
   }
 }
 
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// functional component es6
+const person = () => {
+    return <p>I'm a Person!!</p>
+}
+
+export default person
