2804815c83e6bd64d49a6c357c9222806a7af308
Americo Tomas <americo.tomas@fuseuniversal.com>
2018-07-19 21:04:04 +0100

    adding documentation in pdf

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,24 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  // if state is change, the app is re-rendered
+  state = {
+    persons: [
+      { name: 'Americo', age:'39' },
+      { name: 'Guida', age:'42' },
+      { name: 'Hugo', age:'1' }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I AM A REACT APP!!</h1>
         <h3>This is really working!!</h3>
-        <Person name="Americo" age="39"/>
-        <Person name="Guida" age="42"/>
-        <Person name="Hugo" age="1"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Our Wonderful Baby</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'M A REACT APP!!'))
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 // functional component es6
 const person = (props) => {
-    // deconstructing
+    // destructuring
     const {
         name,
         age
     } = props
 
     // dynamic content
-    return <p>I'm {name} and I am {age} years old!!</p>
+    return (
+        <div>
+            <p>I'm {name} and I am {age} years old!!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person
