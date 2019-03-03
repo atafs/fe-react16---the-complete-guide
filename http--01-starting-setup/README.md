7906ad8b1e7b4537bf0c9bf23e4987f972912edf
Americo Tomas <americo.tomas@fuseuniversal.com>
2019-03-03 03:45:15 +0000

    190. Fetching Data on Update (without Creating Infinite Loops)

--- http--01-starting-setup/README.md
+++ http--01-starting-setup/README.md
@@ -1,3 +1,8 @@
+continue on.. 191. POSTing Data to the Server
+
+// for the api to get data
+https://jsonplaceholder.typicode.com/
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from '../../components/Post/Post'
+import FullPost from '../../components/FullPost/FullPost'
+import NewPost from '../../components/NewPost/NewPost'
+import './Blog.css'
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    selectedPostId: null
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      console.log('response', response)
+
+      //transform the data from the response
+      const posts = response.data.slice(0, 4)
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: 'Americo Tomas!!'
+        }
+      })
+
+      this.setState({
+        posts: updatedPosts
+      })
+    })
+  }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id })
+  }
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      )
+    })
+    console.log('posts', posts)
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    )
+  }
 }
 
-export default Blog;
+export default Blog
